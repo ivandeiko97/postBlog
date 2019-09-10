8f2f503e62dff225d02a39755ca5d6552b04ff73
Ivan Deiko <IvanDeiko97@gmail.com>
2019-09-10 20:30:50 +0300

    Updates

---
+++ precache-manifest.fb15125110b26bb55552dee44e365651.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ba6b0493dc99ff7a0107a5b0c008a6",
+    "url": "/postBlog/index.html"
+  },
+  {
+    "revision": "a26a1685fef7bea92acd",
+    "url": "/postBlog/static/css/main.fa5cf1b1.chunk.css"
+  },
+  {
+    "revision": "990cf674b88788f14004",
+    "url": "/postBlog/static/js/2.9557b778.chunk.js"
+  },
+  {
+    "revision": "a26a1685fef7bea92acd",
+    "url": "/postBlog/static/js/main.5ce5de4f.chunk.js"
+  },
+  {
+    "revision": "1fe3627d46889bffb450",
+    "url": "/postBlog/static/js/runtime~main.077d1304.js"
+  }
+]);
